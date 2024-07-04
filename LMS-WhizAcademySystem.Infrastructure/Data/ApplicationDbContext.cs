cfb77b86f9b7b720f3f477108f24295bbe4880c1
Iwan <iwaniwanow0345@gmail.com>
2024-07-04 16:24:06 +0300

    migrations and db context were created

---
+++ LMS-WhizAcademySystem.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,40 @@
+﻿using LMS_WhizAcademySystem.Infrastructure.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection.Emit;
+
+namespace LMS_WhizAcademySystem.Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+          : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+
+            builder.Entity<Student>()
+              .HasOne(s => s.Relative)
+              .WithOne(r => r.Student)
+              .HasForeignKey<Student>(s => s.RelativeId);
+
+            builder.Entity<Relative>()
+                .HasOne(r => r.Student)
+                .WithOne(s => s.Relative)
+                .HasForeignKey<Relative>(r => r.StudentId);
+
+            builder.Entity<Lesson>()
+               .HasOne(l => l.Student)
+               .WithMany(s => s.Lessons)
+               .HasForeignKey(l => l.StudentId)
+               .OnDelete(DeleteBehavior.Restrict); // Avoid multiple cascade paths
+        }
+
+        public DbSet<Mentor> Mentros { get; set; }
+        public DbSet<Relative> Relatives { get; set; }
+        public DbSet<Lesson> Lessons { get; set; }
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ LMS-WhizAcademySystem.Infrastructure/Migrations/20240704125204_Initialization.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LMS_WhizAcademySystem.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initialization : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Mentros",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PasswordHash = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Mentros", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PriceForHours = table.Column<decimal>(type: "decimal(2,2)", precision: 2, nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    RelativeId = table.Column<int>(type: "int", nullable: true),
+                    MentorId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Students_Mentros_MentorId",
+                        column: x => x.MentorId,
+                        principalTable: "Mentros",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Lessons",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Schedule = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Language = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MentorId = table.Column<int>(type: "int", nullable: false),
+                    StudentId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Lessons", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Lessons_Mentros_MentorId",
+                        column: x => x.MentorId,
+                        principalTable: "Mentros",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Lessons_Students_StudentId",
+                        column: x => x.StudentId,
+                        principalTable: "Students",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Relatives",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    StudentId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Relatives", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Relatives_Students_StudentId",
+                        column: x => x.StudentId,
+                        principalTable: "Students",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Lessons_MentorId",
+                table: "Lessons",
+                column: "MentorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Lessons_StudentId",
+                table: "Lessons",
+                column: "StudentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Relatives_StudentId",
+                table: "Relatives",
+                column: "StudentId",
+                unique: true,
+                filter: "[StudentId] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Students_MentorId",
+                table: "Students",
+                column: "MentorId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Lessons");
+
+            migrationBuilder.DropTable(
+                name: "Relatives");
+
+            migrationBuilder.DropTable(
+                name: "Students");
+
+            migrationBuilder.DropTable(
+                name: "Mentros");
+        }
+    }
+}
