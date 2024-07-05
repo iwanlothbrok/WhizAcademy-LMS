c47d5052d32a1d5296439eaa70b1f52d7c2f4c17
Iwan <iwaniwanow0345@gmail.com>
2024-07-05 17:57:02 +0300

    mentors db file

---
+++ lms-whizacademysystem.client/src/components/database/mentorsCards.js
@@ -0,0 +1,15 @@
+import add from '../../assets/images/graffits/15.png'
+import all from '../../assets/images/graffits/16.png'
+import remove from '../../assets/images/graffits/17.png'
+import edit from '../../assets/images/graffits/18.png'
+
+
+const mentorsCards = [
+    { id: 1, src: add, link: '/page1', information: 'Добави' },
+    { id: 2, src: all, link: '/page2', information: 'Ментори' },
+    { id: 3, src: remove, link: '/page3', information: 'Изтрий' },
+    { id: 4, src: edit, link: '/page4', information: 'Промени' },
+];
+
+
+export default mentorsCards;
