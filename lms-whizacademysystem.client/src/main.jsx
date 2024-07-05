02f5dc87f1e3fb02cf7786ac51cef118955a967b
Iwan <iwaniwanow0345@gmail.com>
2024-07-05 17:57:17 +0300

    routing was imported

--- lms-whizacademysystem.client/src/main.jsx
+++ lms-whizacademysystem.client/src/main.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import App from './App.jsx';
+import './index.css';
+import './tailwind.css'; // Tailwind styles
+import OperationsCard from './components/OperationsCard.jsx';
+import mentorsCards from './components/database/mentorsCards.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/mentors" element={<OperationsCard cards={mentorsCards} />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>
+);
