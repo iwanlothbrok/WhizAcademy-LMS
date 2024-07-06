109dccfbec34ca49cc47d7b2409f3cad1108a94c
Iwan <iwaniwanow0345@gmail.com>
2024-07-06 15:21:37 +0300

    mentor dto added

---
+++ LMS-WhizAcademySystem.Server/Models/MentorFormDTO.cs
@@ -0,0 +1,17 @@
+﻿namespace LMS_WhizAcademySystem.Server.Models
+{
+	using System.ComponentModel.DataAnnotations;
+
+	public class MentorFormDTO
+	{
+		[Required, StringLength(50)]
+		public string Name { get; set; } = null!;
+
+		[EmailAddress]
+		[Required]
+		public string Email { get; set; } = null!;
+
+		[Required]
+		public string PasswordHash { get; set; } = null!;
+	}
+}
