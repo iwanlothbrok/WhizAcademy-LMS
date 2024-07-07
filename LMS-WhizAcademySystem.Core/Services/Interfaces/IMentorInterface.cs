ead1304d053c802f5aa3dfd0a952459636a225de
MiroslavIvanov8 <ivanov8.work@gmail.com>
2024-07-07 18:51:08 +0300

    created IMentorService and started implementation

---
+++ LMS-WhizAcademySystem.Core/Services/Interfaces/IMentorInterface.cs
@@ -0,0 +1,11 @@
+﻿using LMS_WhizAcademySystem.Server.Models;
+
+namespace LMS_WhizAcademySystem.Core.Services.Interfaces
+{
+    public interface IMentorService
+    {
+        public void Add(MentorFormDTO mentorForm);
+
+        public IEnumerable<MentorInformationDTO> GetAll();
+    }
+}
---
+++ LMS-WhizAcademySystem.Core/Services/MentorService.cs
@@ -0,0 +1,57 @@
+﻿using LMS_WhizAcademySystem.Core.Services.Interfaces;
+using LMS_WhizAcademySystem.Infrastructure.Data;
+using LMS_WhizAcademySystem.Infrastructure.Models;
+using LMS_WhizAcademySystem.Server.Models;
+
+namespace LMS_WhizAcademySystem.Core.Services
+{
+    public class MentorService : IMentorService
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public MentorService(ApplicationDbContext dbContext)
+        {
+            this._dbContext = dbContext;
+        }
+        public void Add(MentorFormDTO mentorForm)
+        { 
+
+            if (string.IsNullOrEmpty(mentorForm.Name) || string.IsNullOrWhiteSpace(mentorForm.Name) ||
+                string.IsNullOrEmpty(mentorForm.Email) || string.IsNullOrWhiteSpace(mentorForm.Email) ||
+                string.IsNullOrEmpty(mentorForm.Password) || string.IsNullOrWhiteSpace(mentorForm.Password))
+            {
+                throw new Exception("Null value passed in form.");
+            }
+
+            
+            
+            Mentor mentor = new Mentor()
+            {
+                Name = mentorForm.Name,
+                Email = mentorForm.Email,
+                PasswordHash = "",
+                Students = new List<Student>()
+            };
+
+            _dbContext.Mentors.Add(mentor);
+            _dbContext.SaveChanges();
+
+        }
+
+        public IEnumerable<MentorInformationDTO> GetAll()
+        {
+            IEnumerable<MentorInformationDTO> mentors = _dbContext.Mentors.Select(m => new MentorInformationDTO()
+            {
+                Name = m.Name,
+                Email = m.Email,
+                LessonsCount = 69,
+                EarnedMoney = 123.4m,
+                LastLessonDate = DateTime.Today,
+                Students = m.Students
+            });
+
+            return mentors;
+        }
+        
+    }
+}
