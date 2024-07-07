c18fb91de008a6ee03b662743e14138fc3452fa5
Iwan <iwaniwanow0345@gmail.com>
2024-07-07 21:42:26 +0300

    mentor service refactored

--- LMS-WhizAcademySystem.Core/Services/MentorService.cs
+++ LMS-WhizAcademySystem.Core/Services/MentorService.cs
@@ -1,57 +1,57 @@
-﻿using LMS_WhizAcademySystem.Core.Services.Interfaces;
-using LMS_WhizAcademySystem.Infrastructure.Data;
-using LMS_WhizAcademySystem.Infrastructure.Models;
-using LMS_WhizAcademySystem.Server.Models;
-
-namespace LMS_WhizAcademySystem.Core.Services
+﻿namespace LMS_WhizAcademySystem.Core.Services
 {
-    public class MentorService : IMentorService
-    {
-        private readonly ApplicationDbContext _dbContext;
+	using LMS_WhizAcademySystem.Core.Services.Interfaces;
+	using LMS_WhizAcademySystem.Infrastructure.Data;
+	using LMS_WhizAcademySystem.Infrastructure.Models;
+	using LMS_WhizAcademySystem.Server.Models;
 
-        public MentorService(ApplicationDbContext dbContext)
-        {
-            this._dbContext = dbContext;
-        }
-        public void Add(MentorFormDTO mentorForm)
-        { 
+	public class MentorService : IMentorService
+	{
+		private readonly ApplicationDbContext _dbContext;
 
-            if (string.IsNullOrEmpty(mentorForm.Name) || string.IsNullOrWhiteSpace(mentorForm.Name) ||
-                string.IsNullOrEmpty(mentorForm.Email) || string.IsNullOrWhiteSpace(mentorForm.Email) ||
-                string.IsNullOrEmpty(mentorForm.Password) || string.IsNullOrWhiteSpace(mentorForm.Password))
-            {
-                throw new Exception("Null value passed in form.");
-            }
+		public MentorService(ApplicationDbContext dbContext)
+		{
+			this._dbContext = dbContext;
+		}
+		public void Add(MentorFormDTO mentorForm)
+		{
 
-            
-            
-            Mentor mentor = new Mentor()
-            {
-                Name = mentorForm.Name,
-                Email = mentorForm.Email,
-                PasswordHash = "",
-                Students = new List<Student>()
-            };
+			if (string.IsNullOrEmpty(mentorForm.Name) || string.IsNullOrWhiteSpace(mentorForm.Name) ||
+				string.IsNullOrEmpty(mentorForm.Email) || string.IsNullOrWhiteSpace(mentorForm.Email) ||
+				string.IsNullOrEmpty(mentorForm.Password) || string.IsNullOrWhiteSpace(mentorForm.Password))
+			{
+				throw new Exception("Null value passed in form.");
+			}
 
-            _dbContext.Mentors.Add(mentor);
-            _dbContext.SaveChanges();
+			Mentor mentor = new Mentor()
+			{
+				Name = mentorForm.Name,
+				Email = mentorForm.Email,
+				PasswordHash = mentorForm.Password,
+				Students = new List<Student>()
+			};
 
-        }
+			_dbContext.Mentors.Add(mentor);
+			_dbContext.SaveChanges();
+		}
 
-        public IEnumerable<MentorInformationDTO> GetAll()
-        {
-            IEnumerable<MentorInformationDTO> mentors = _dbContext.Mentors.Select(m => new MentorInformationDTO()
-            {
-                Name = m.Name,
-                Email = m.Email,
-                LessonsCount = 69,
-                EarnedMoney = 123.4m,
-                LastLessonDate = DateTime.Today,
-                Students = m.Students
-            });
+		public IEnumerable<MentorInformationDTO> GetAll()
+		{
+			IEnumerable<MentorInformationDTO> mentors = _dbContext.Mentors.Select(m => new MentorInformationDTO()
+			{
+				Name = m.Name,
+				Email = m.Email,
+				LessonsCount = 69,
+				EarnedMoney = 123.4m,
+				LastLessonDate = DateTime.Today,
 
-            return mentors;
-        }
-        
-    }
+				// STUDENTS WILL BE EVERYTIME EMPTY LIST
+				// - GET THE STUDENTS WITH FK - MENTOR ID	 
+				Students = new List<Student>() 
+			});
+
+			return mentors;
+		}
+
+	}
 }
