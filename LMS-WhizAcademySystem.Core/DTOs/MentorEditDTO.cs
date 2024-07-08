d74520f45a458d5a27ffd8bc876694950c81d963
MiroslavIvanov8 <ivanov8.work@gmail.com>
2024-07-08 13:57:13 +0300

    created MentorEditDTO

---
+++ LMS-WhizAcademySystem.Core/DTOs/MentorEditDTO.cs
@@ -0,0 +1,9 @@
+﻿using LMS_WhizAcademySystem.Server.Models;
+
+namespace LMS_WhizAcademySystem.Core.DTOs
+{
+    public class MentorEditDTO : MentorFormDTO
+    {
+        public int Id { get; set; }
+    }
+}
