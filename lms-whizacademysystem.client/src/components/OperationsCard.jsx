e07b14f8d6c258164c0aa6bbe7a6b7824b8b2abe
Iwan <iwaniwanow0345@gmail.com>
2024-07-08 11:33:29 +0300

    improving operationscard component
    added keys to list and also lazy loading to the photos

--- lms-whizacademysystem.client/src/components/OperationsCard.jsx
+++ lms-whizacademysystem.client/src/components/OperationsCard.jsx
@@ -1,24 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-export default function OperationsCard({cards}) {
-
-    console.log(cards);
+export default function OperationsCard({ cards }) {
     return (
-        <div className="w-screen  h-screen flex justify-center items-center">
-
-            <div className="grid grid-cols-2 m-6 sm:grid-cols-4 md:grid-cols-2 gap-2  text-textGreen">
-                {cards.map(x => (
-                    <div >
-
-                        <div key={x.id} className="border px-5 m-1 mb-2 p-8 rounded overflow-hidden flex flex-col items-center  transform transition-transform duration-300 hover:scale-110 bg-black">
-                            <Link to={x.link}>
-                                <img src={x.src} alt={`Image ${x.id}`} className="w-56 h-auto homeContainerImage" />
-                            </Link>
-                            <h3 className='font-bold font-sans text-2xl mt-1'>{x.information}</h3>
+        <div className="w-screen h-screen flex justify-center items-center">
+            <div className="grid grid-cols-2 m-6 sm:grid-cols-4 md:grid-cols-2 gap-2 text-textGreen">
+                {cards.map(card => (
+                    <Link key={card.id} to={card.link}>
+                        <div className="border px-5 m-1 mb-2 p-8 rounded overflow-hidden flex flex-col items-center transform transition-transform duration-300 hover:scale-110 bg-black">
+                            <img src={card.src} alt={card.information} loading="lazy" className="w-56 h-auto homeContainerImage" />
+                            <h3 className="font-bold font-sans text-2xl mt-1">{card.information}</h3>
                         </div>
-
-                    </div>
+                    </Link>
                 ))}
             </div>
         </div>
