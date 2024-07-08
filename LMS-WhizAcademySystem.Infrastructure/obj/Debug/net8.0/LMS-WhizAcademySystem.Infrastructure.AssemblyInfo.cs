d0542c3676ea0afb2f0a0b98811a45e142b14826
MiroslavIvanov8 <ivanov8.work@gmail.com>
2024-07-08 13:58:02 +0300

    rest bin and obj files

--- LMS-WhizAcademySystem.Infrastructure/obj/Debug/net8.0/LMS-WhizAcademySystem.Infrastructure.AssemblyInfo.cs
+++ LMS-WhizAcademySystem.Infrastructure/obj/Debug/net8.0/LMS-WhizAcademySystem.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LMS-WhizAcademySystem.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9922bbadbca0c0ec7645fa0504df301a6ab991ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("LMS-WhizAcademySystem.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LMS-WhizAcademySystem.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
