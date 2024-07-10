8b0d9e8a17c392cb7fa74f8c3b67a1431d1b351e
Iwan <iwaniwanow0345@gmail.com>
2024-07-10 10:33:22 +0300

    adding students in database with api is working

--- LMS-WhizAcademySystem.Server/Controllers/StudentsApiController.cs
+++ LMS-WhizAcademySystem.Server/Controllers/StudentsApiController.cs
@@ -1,18 +1,30 @@
 ﻿namespace LMS_WhizAcademySystem.Server.Controllers
 {
+    using AutoMapper;
     using LMS_WhizAcademySystem.Core.DTOs;
+    using LMS_WhizAcademySystem.Infrastructure.Data;
+    using LMS_WhizAcademySystem.Infrastructure.Models;
     using Microsoft.AspNetCore.Mvc;
+    using static Org.BouncyCastle.Math.EC.ECCurve;
 
     [Route("api/students")]
     [ApiController]
     public class StudentsApiController : ControllerBase
     {
+        private readonly ApplicationDbContext data;
+        private readonly IMapper mapper;
+
+        public StudentsApiController(ApplicationDbContext data, IMapper mapper)
+        {
+            this.data = data;
+            this.mapper = mapper;
+        }
 
         // TODO: change the dto | add the student service
         [HttpPost("add")] // api/students/add
-        public async Task<IActionResult> AddStudent([FromForm] StudentFormDTO mentor, IFormFile roadmap) //[
+        public async Task<IActionResult> AddStudent([FromForm] StudentFormDTO student, IFormFile roadmap) //[
         {
-            if (mentor == null)
+            if (student == null)
             {
                 return BadRequest("Mentor is null.");
             }
@@ -27,10 +39,17 @@
                 using (var memoryStream = new MemoryStream())
                 {
                     await roadmap.CopyToAsync(memoryStream);
-                    mentor.Roadmap = memoryStream.ToArray();
+                    student.Roadmap = memoryStream.ToArray();
                 }
             }
 
+
+            Student stu = mapper.Map<Student>(student);
+
+            // TODO: REMOVE DATA
+
+            this.data.Students.Add(stu);
+            this.data.SaveChanges();
             //try
             //{
             //    _mentorService.Add(mentor);
