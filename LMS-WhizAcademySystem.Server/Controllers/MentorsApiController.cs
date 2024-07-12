caf1b544b0ecd8f3afdac834dfbcad11383bc2d0
Iwan <iwaniwanow0345@gmail.com>
2024-07-12 10:57:14 +0300

    refactoring apis

--- LMS-WhizAcademySystem.Server/Controllers/MentorsApiController.cs
+++ LMS-WhizAcademySystem.Server/Controllers/MentorsApiController.cs
@@ -1,121 +1,120 @@
 ﻿namespace LMS_WhizAcademySystem.Server.Controllers
 {
-    using LMS_WhizAcademySystem.Core.DTOs;
-    using Core.Services.Interfaces;
-    using Microsoft.AspNetCore.Mvc;
-    using Models;
+	using LMS_WhizAcademySystem.Core.DTOs;
+	using Core.Services.Interfaces;
+	using Microsoft.AspNetCore.Mvc;
+	using Models;
 
-    [Route("api/mentors")]
-    [ApiController]
-    public class MentorsApiController : ControllerBase
-    {
-        private readonly IMentorService _mentorService;
+	[Route("api/mentors")]
+	[ApiController]
+	public class MentorsApiController : ControllerBase
+	{
+		private readonly IMentorService _mentorService;
 
-        public MentorsApiController(IMentorService mentorService)
-        {
-            this._mentorService = mentorService;
-        }
+		public MentorsApiController(IMentorService mentorService)
+		{
+			this._mentorService = mentorService;
+		}
 
-        // GET: api/mentors/all
-        [HttpGet("all")]
-        public IEnumerable<MentorInformationDTO> Get()
-        {
-            IEnumerable<MentorInformationDTO> mentors = _mentorService.GetAll();
+		// GET: api/mentors/all
+		[HttpGet("all")]
+		public IEnumerable<MentorInformationDTO> Get()
+		{
+			IEnumerable<MentorInformationDTO> mentors = _mentorService.GetAll();
 
-            return mentors;
-        }
+			return mentors;
+		}
 
-        [HttpPost("add")] // api/mentors/add
-        public IActionResult Post([FromBody] MentorFormDTO mentor) //[
-        {
-            if (mentor == null)
-            {
-                return BadRequest("Mentor is null.");
-            }
+		[HttpPost("add")] // api/mentors/add
+		public IActionResult Post([FromBody] MentorFormDTO mentor) //[
+		{
+			if (mentor == null)
+			{
+				return BadRequest("Mentor is null.");
+			}
 
-            if (!ModelState.IsValid)
-            {
-                return BadRequest("Invalid ModelState.");
-            }
+			if (!ModelState.IsValid)
+			{
+				return BadRequest("Invalid ModelState.");
+			}
 
-            try
-            {
-                _mentorService.Add(mentor);
-            }
-            catch (Exception)
-            {
-                return BadRequest();
-            }
+			try
+			{
+				_mentorService.Add(mentor);
+			}
+			catch (Exception)
+			{
+				return BadRequest();
+			}
 
-            return Ok("Mentor added successfully");
-        }
+			return Ok("Mentor added successfully");
+		}
 
 
-        // GET api/mentors/5
-        [HttpGet("{id}")]
-        public IActionResult Get(int id)
-        {
-            var mentor = _mentorService.GetById(id);
+		// GET api/mentors/5
+		[HttpGet("{id}")]
+		public IActionResult Get(int id)
+		{
+			var mentor = _mentorService.GetById(id);
 
-            if (mentor == null)
-            {
-                return BadRequest();
-            }
+			if (mentor == null)
+			{
+				return BadRequest();
+			}
 
-            return Ok(mentor);
-        }
+			return Ok(mentor);
+		}
 
-        [HttpPut("edit")] // api/mentors/edit
-        public IActionResult Edit([FromBody] MentorEditDTO editForm)
-        {
-            if (editForm == null)
-            {
-                return BadRequest();
-            }
+		[HttpPut("edit")] // api/mentors/edit
+		public IActionResult Edit([FromBody] MentorEditDTO editForm)
+		{
+			if (editForm == null)
+			{
+				return BadRequest();
+			}
 
-            if (!ModelState.IsValid)
-            {
-                return BadRequest();
-            }
+			if (!ModelState.IsValid)
+			{
+				return BadRequest();
+			}
 
-            try
-            {
-                _mentorService.Edit(editForm);
-            }
-            catch (Exception e)
-            {
-                return BadRequest("Error in mentor update");
-            }
+			try
+			{
+				_mentorService.Edit(editForm);
+			}
+			catch (Exception e)
+			{
+				return BadRequest("Error in mentor update");
+			}
 
-            return Ok("Mentor updated successfully");
-        }
+			return Ok("Mentor updated successfully");
+		}
 
-        //// PUT api/<MentorsApiController>/5
-        //[HttpPut("{id}")]
-        //public void Put(int id, [FromBody] string value)
-        //{
-        //}
+		//// PUT api/<MentorsApiController>/5
+		//[HttpPut("{id}")]
+		//public void Put(int id, [FromBody] string value)
+		//{
+		//}
 
-        // DELETE api/mentor/5
-        [HttpDelete("{id}")]
-        public IActionResult Delete(int id)
-        {
-            if (id < 0)
-            {
-                return BadRequest();
-            }
+		// DELETE api/mentor/5
+		[HttpDelete("{id}")]
+		public IActionResult Delete(int id)
+		{
+			if (id < 0)
+			{
+				return BadRequest();
+			}
 
-            try
-            {
-                _mentorService.Delete(id);
-            }
-            catch (Exception)
-            {
+			try
+			{
+				_mentorService.Delete(id);
+			}
+			catch (Exception)
+			{
+				return BadRequest();
+			}
 
-                return BadRequest();
-            }
-
-            return Ok();
-        }
-    }
+			return Ok();
+		}
+	}
 }
--- LMS-WhizAcademySystem.Server/Controllers/StudentsApiController.cs
+++ LMS-WhizAcademySystem.Server/Controllers/StudentsApiController.cs
@@ -1,210 +1,206 @@
 ﻿namespace LMS_WhizAcademySystem.Server.Controllers
 {
-    using AutoMapper;
-    using LMS_WhizAcademySystem.Core.DTOs;
-    using LMS_WhizAcademySystem.Infrastructure.Data;
-    using LMS_WhizAcademySystem.Infrastructure.Models;
-    using Microsoft.AspNetCore.Mvc;
-    using Microsoft.EntityFrameworkCore;
-    using OfficeOpenXml;
+	using AutoMapper;
+	using LMS_WhizAcademySystem.Core.DTOs;
+	using LMS_WhizAcademySystem.Infrastructure.Data;
+	using LMS_WhizAcademySystem.Infrastructure.Models;
+	using Microsoft.AspNetCore.Mvc;
+	using Microsoft.EntityFrameworkCore;
+	using OfficeOpenXml;
 
-    [Route("api/students")]
-    [ApiController]
-    public class StudentsApiController : ControllerBase
-    {
-        private readonly ApplicationDbContext data;
-        private readonly IMapper mapper;
+	[Route("api/students")]
+	[ApiController]
+	public class StudentsApiController : ControllerBase
+	{
+		private readonly ApplicationDbContext data;
+		private readonly IMapper mapper;
 
-        public StudentsApiController(ApplicationDbContext data, IMapper mapper)
-        {
-            this.data = data;
-            this.mapper = mapper;
-        }
+		public StudentsApiController(ApplicationDbContext data, IMapper mapper)
+		{
+			this.data = data;
+			this.mapper = mapper;
+		}
 
-        // TODO: change the dto | add the student service
-        [HttpPost("add")] // api/students/add
-        public async Task<IActionResult> AddStudent([FromForm] StudentFormDTO student, IFormFile roadmap) //[
-        {
-            if (student == null)
-            {
-                return BadRequest("Mentor is null.");
-            }
+		// TODO: change the dto | add the student service
+		[HttpPost("add")] // api/students/add
+		public async Task<IActionResult> AddStudent([FromForm] StudentFormDTO student, IFormFile roadmap) //[
+		{
+			if (student == null)
+			{
+				return BadRequest("Mentor is null.");
+			}
 
-            if (!ModelState.IsValid)
-            {
-                return BadRequest("Invalid ModelState.");
-            }
+			if (!ModelState.IsValid)
+			{
+				return BadRequest("Invalid ModelState.");
+			}
 
-            if (roadmap != null && roadmap.Length > 0)
-            {
-                using (var memoryStream = new MemoryStream())
-                {
-                    await roadmap.CopyToAsync(memoryStream);
-                    student.Roadmap = memoryStream.ToArray();
-                }
-            }
-
-
-            Student stu = mapper.Map<Student>(student);
-
-            // TODO: REMOVE DATA
-
-            await this.data.Students.AddAsync(stu);
-            await this.data.SaveChangesAsync();
-            //try
-            //{
-            //    _mentorService.Add(mentor);
-            //}
-            //catch (Exception)
-            //{
-            //    return BadRequest();
-            //}
-
-            return Ok("Mentor added successfully");
-        }
-
-        [HttpGet("all")]
-        public List<Student> GetAllStudents()
-        {
-            List<Student> students = this.data.Students.Include(x => x.Mentor).Include(x => x.Relative).ToList();
-
-            List<StudentFormDTO> stu = mapper.Map<List<StudentFormDTO>>(students);
+			if (roadmap != null && roadmap.Length > 0)
+			{
+				using (var memoryStream = new MemoryStream())
+				{
+					await roadmap.CopyToAsync(memoryStream);
+					student.Roadmap = memoryStream.ToArray();
+				}
+			}
 
 
-            //foreach (var x in students)
-            //{
-            //    x.Roadmap = [];
-            //}
-            return students;
-        }
+			Student stu = mapper.Map<Student>(student);
 
-        [HttpDelete("{id}")]
-        public IActionResult Delete(int id)
-        {
-            if (id < 0)
-            {
-                return BadRequest();
-            }
+			// TODO: REMOVE DATA
 
-            try
-            {
-                var student = this.data.Students.FirstOrDefault(data => data.Id == id);
+			await this.data.Students.AddAsync(stu);
+			await this.data.SaveChangesAsync();
+			//try
+			//{
+			//    _mentorService.Add(mentor);
+			//}
+			//catch (Exception)
+			//{
+			//    return BadRequest();
+			//}
 
-                if (student == null)
-                {
-                    return BadRequest();
-                }
+			return Ok("Mentor added successfully");
+		}
 
-                this.data.Remove(student);
-                this.data.SaveChanges();
-            }
-            catch (Exception)
-            {
-                return BadRequest();
-            }
+		[HttpGet("all")]
+		public List<Student> GetAllStudents()
+		{
+			List<Student> students = this.data.Students.Include(x => x.Mentor).Include(x => x.Relative).ToList();
 
-            return Ok();
-        }
+			List<StudentFormDTO> stu = mapper.Map<List<StudentFormDTO>>(students);
 
-        [HttpGet("get/roadmap/{id}")]
-        public IActionResult GetRoadmap(int id) // int id
-        {
-            try
-            {
-                // Fetch the byte array from the database
-                var excelBytes = data.Students.FirstOrDefault(x => x.Id == id)?.Roadmap;
-                if (excelBytes == null)
-                {
-                    return NotFound(new { message = "Student not found or roadmap is empty." });
-                }
+			return students;
+		}
 
-                // Save the byte array as an Excel file
-                var filePath = Path.Combine(Directory.GetCurrentDirectory(), "UploadedFile.xlsx");
-                System.IO.File.WriteAllBytes(filePath, excelBytes);
+		[HttpDelete("{id}")]
+		public IActionResult Delete(int id)
+		{
+			if (id < 0)
+			{
+				return BadRequest();
+			}
+
+			try
+			{
+				var student = this.data.Students.FirstOrDefault(data => data.Id == id);
+
+				if (student == null)
+				{
+					return BadRequest();
+				}
+
+				this.data.Remove(student);
+				this.data.SaveChanges();
+			}
+			catch (Exception)
+			{
+				return BadRequest();
+			}
+
+			return Ok();
+		}
+
+		[HttpGet("get/roadmap/{id}")]
+		public IActionResult GetRoadmap(int id) // int id
+		{
+			try
+			{
+				// Fetch the byte array from the database
+				var excelBytes = data.Students.FirstOrDefault(x => x.Id == id)?.Roadmap;
+				if (excelBytes == null)
+				{
+					return NotFound(new { message = "Student not found or roadmap is empty." });
+				}
+
+				// Save the byte array as an Excel file
+				var filePath = Path.Combine(Directory.GetCurrentDirectory(), "UploadedFile.xlsx");
+				System.IO.File.WriteAllBytes(filePath, excelBytes);
 
 
-                ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+				ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
 
-                // Process the Excel file and convert it to JSON
-                var jsonData = new List<Dictionary<string, object>>();
-                using (var package = new ExcelPackage(new FileInfo(filePath)))
-                {
-                    var worksheet = package.Workbook.Worksheets[0];
-                    var rowCount = worksheet.Dimension.Rows;
-                    var colCount = worksheet.Dimension.Columns;
+				// Process the Excel file and convert it to JSON
+				var jsonData = new List<Dictionary<string, object>>();
+				using (var package = new ExcelPackage(new FileInfo(filePath)))
+				{
+					var worksheet = package.Workbook.Worksheets[0];
+					var rowCount = worksheet.Dimension.Rows;
+					var colCount = worksheet.Dimension.Columns;
 
-                    for (int row = 2; row <= rowCount; row++) // Assuming the first row is the header
-                    {
-                        var rowDict = new Dictionary<string, object>();
-                        for (int col = 1; col <= colCount; col++)
-                        {
-                            var key = worksheet.Cells[1, col].Text; // Header as key
-                            var value = worksheet.Cells[row, col].Text;
-                            rowDict[key] = string.IsNullOrWhiteSpace(value) ? "NaN" : value; // Replace empty cells with "NaN"
-                        }
-                        jsonData.Add(rowDict);
-                    }
-                }
+					for (int row = 2; row <= rowCount; row++) // Assuming the first row is the header
+					{
+						var rowDict = new Dictionary<string, object>();
+						for (int col = 1; col <= colCount; col++)
+						{
+							var key = worksheet.Cells[1, col].Text; // Header as key
+							var value = worksheet.Cells[row, col].Text;
+							rowDict[key] = string.IsNullOrWhiteSpace(value) ? "NaN" : value; // Replace empty cells with "NaN"
+						}
+						jsonData.Add(rowDict);
+					}
+				}
 
-                return Ok(jsonData);
-            }
-            catch (System.Exception ex)
-            {
-                return BadRequest(new { message = ex.Message });
-            }
-        }
-        [HttpPost("update/{id}")]
-        public IActionResult UpdateExcelData(int id, [FromBody] List<Dictionary<string, string>> updatedData)
-        {
-            try
-            {
-                // Fetch the byte array from the database
-                var excelBytes = data.Students.FirstOrDefault(x => x.Id == id)?.Roadmap;
-                if (excelBytes == null)
-                {
-                    return NotFound(new { message = "Student not found or roadmap is empty." });
-                }
+				return Ok(jsonData);
+			}
+			catch (System.Exception ex)
+			{
+				return BadRequest(new { message = ex.Message });
+			}
+		}
 
-                // Set the license context
-                ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+		[HttpPost("update/{id}")]
+		public IActionResult UpdateExcelData(int id, [FromBody] List<Dictionary<string, string>> updatedData)
+		{
+			try
+			{
+				// Fetch the byte array from the database
+				var excelBytes = data.Students.FirstOrDefault(x => x.Id == id)?.Roadmap;
+				if (excelBytes == null)
+				{
+					return NotFound(new { message = "Student not found or roadmap is empty." });
+				}
 
-                // Update the Excel file with the new data
-                using (var package = new ExcelPackage(new MemoryStream(excelBytes)))
-                {
-                    var worksheet = package.Workbook.Worksheets[0];
+				// Set the license context
+				ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
 
-                    // Update the worksheet with the new data
-                    for (int row = 0; row < updatedData.Count; row++)
-                    {
-                        var rowDict = updatedData[row];
-                        int col = 1;
-                        foreach (var key in rowDict.Keys)
-                        {
-                            worksheet.Cells[row + 2, col].Value = rowDict[key];
-                            col++;
-                        }
-                    }
+				// Update the Excel file with the new data
+				using (var package = new ExcelPackage(new MemoryStream(excelBytes)))
+				{
+					var worksheet = package.Workbook.Worksheets[0];
 
-                    // Save the updated Excel file
-                    var memoryStream = new MemoryStream();
-                    package.SaveAs(memoryStream);
-                    excelBytes = memoryStream.ToArray();
+					// Update the worksheet with the new data
+					for (int row = 0; row < updatedData.Count; row++)
+					{
+						var rowDict = updatedData[row];
+						int col = 1;
+						foreach (var key in rowDict.Keys)
+						{
+							worksheet.Cells[row + 2, col].Value = rowDict[key];
+							col++;
+						}
+					}
 
-                    // Update the byte array in the database
-                    var student = data.Students.FirstOrDefault(x => x.Id == id);
-                    if (student != null)
-                    {
-                        student.Roadmap = excelBytes;
-                        data.SaveChanges();
-                    }
-                }
+					// Save the updated Excel file
+					var memoryStream = new MemoryStream();
+					package.SaveAs(memoryStream);
+					excelBytes = memoryStream.ToArray();
 
-                return Ok(new { message = "Excel file updated successfully." });
-            }
-            catch (System.Exception ex)
-            {
-                return BadRequest(new { message = ex.Message });
-            }
-        }
-    }
+					// Update the byte array in the database
+					var student = data.Students.FirstOrDefault(x => x.Id == id);
+					if (student != null)
+					{
+						student.Roadmap = excelBytes;
+						data.SaveChanges();
+					}
+				}
+
+				return Ok(new { message = "Excel file updated successfully." });
+			}
+			catch (System.Exception ex)
+			{
+				return BadRequest(new { message = ex.Message });
+			}
+		}
+	}
 }
