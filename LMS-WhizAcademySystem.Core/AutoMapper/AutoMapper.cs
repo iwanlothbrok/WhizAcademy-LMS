e8046658b5af0e55dcda5997c35facd4594924dc
MiroslavIvanov8 <ivanov8.work@gmail.com>
2024-07-13 14:31:32 +0300

    Automapper moved to Core

---
+++ LMS-WhizAcademySystem.Core/AutoMapper/AutoMapper.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using LMS_WhizAcademySystem.Core.DTOs;
+using LMS_WhizAcademySystem.Infrastructure.Models;
+using LMS_WhizAcademySystem.Server.Models;
+
+namespace LMS_WhizAcademySystem.Core.AutoMapper
+{
+	public class AutoMapper : Profile
+	{
+		public AutoMapper()
+		{
+			CreateMap<Student, StudentFormDTO>();
+			CreateMap<StudentFormDTO, Student>();
+
+			CreateMap<Mentor, MentorFormDTO>();
+			CreateMap<MentorFormDTO, Mentor>()
+			.ForMember(dest => dest.PasswordHash, opt => opt.MapFrom(src => src.Password));
+
+		}
+	}
+}
