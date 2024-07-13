b0fba462269af97d02e93e84397384dd98499b44
Iwan <iwaniwanow0345@gmail.com>
2024-07-13 20:59:58 +0300

    payment api controller was created

--- LMS-WhizAcademySystem.Core/obj/Debug/net8.0/LMS-WhizAcademySystem.Core.AssemblyInfo.cs
+++ LMS-WhizAcademySystem.Core/obj/Debug/net8.0/LMS-WhizAcademySystem.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LMS-WhizAcademySystem.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d05afafc01d3d383333bb8a071131eb4a7c08e7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("LMS-WhizAcademySystem.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LMS-WhizAcademySystem.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
