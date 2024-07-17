9521fa99b568b522b387a5c3dac33a8dcef58531
Iwan <iwaniwanow0345@gmail.com>
2024-07-17 16:22:15 +0300

    payment form dto was changed

--- LMS-WhizAcademySystem.Core/DTOs/PaymentFormDTO.cs
+++ LMS-WhizAcademySystem.Core/DTOs/PaymentFormDTO.cs
@@ -2,12 +2,17 @@
 {
 	public class PaymentFormDTO
 	{
-		public decimal Amount { get; set; }
-		public int LessonsCount { get; set; }
-		public DateTime FirstLessonDate { get; set; }
-		public DateTime LastLessonDate { get; set; }
-		public DateTime PaymentDate { get; set; }
-		public int MentorId { get; set; }
+        public decimal Amount { get; set; }
+
+        public int PayedLessons { get; set; }
+
+        public int LessonsCompleted { get; set; }
+
+        public DateTime? FirstLessonDate { get; set; }
+
+        public DateTime PaymentDate { get; set; }
+
+        public int MentorId { get; set; }
 		public int StudentId { get; set; }
 	}
 }
