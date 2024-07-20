accf26bdfc0b81c17cac41c48fe9a88ca3e67a8f
Iwan <iwaniwanow0345@gmail.com>
2024-07-20 16:35:23 +0300

    event api controller

---
+++ LMS-WhizAcademySystem.Server/Controllers/EventApiController.cs
@@ -0,0 +1,10 @@
+﻿namespace LMS_WhizAcademySystem.Server.Controllers
+{
+	using Microsoft.AspNetCore.Mvc;
+
+	[Route("api/events")]
+	[ApiController]
+	public class EventApiController : ControllerBase
+	{
+	}
+}
