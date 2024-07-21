eec77072295028cd0447be67c1bddcbbb627945b
Iwan <iwaniwanow0345@gmail.com>
2024-07-21 22:06:02 +0300

    event controller was used - add method was implemented

--- LMS-WhizAcademySystem.Server/Controllers/EventApiController.cs
+++ LMS-WhizAcademySystem.Server/Controllers/EventApiController.cs
@@ -1,10 +1,48 @@
 ﻿namespace LMS_WhizAcademySystem.Server.Controllers
 {
+	using AutoMapper;
+	using LMS_WhizAcademySystem.Core.DTOs;
+	using LMS_WhizAcademySystem.Infrastructure.Data;
+	using LMS_WhizAcademySystem.Infrastructure.Models;
 	using Microsoft.AspNetCore.Mvc;
 
-	[Route("api/events")]
+	[Route("api/event")]
 	[ApiController]
 	public class EventApiController : ControllerBase
 	{
+		public ApplicationDbContext data;
+		public IMapper mapper;
+
+		public EventApiController(ApplicationDbContext data)
+		{
+			this.data = data;
+		}
+
+		[HttpPost("add")] // api/mentors/add FROM QUERY
+		public IActionResult Post([FromBody] EventFormDTO eventForm) //[
+		{
+			if (eventForm == null)
+			{
+				return BadRequest("Mentor is null.");
+			}
+
+			if (!ModelState.IsValid)
+			{
+				return BadRequest("Invalid ModelState.");
+			}
+
+			try
+			{
+				var eventModel = mapper.Map<Event>(eventForm);		
+
+				data.Events.Add(eventModel);
+			}
+			catch (Exception)
+			{
+				return BadRequest();
+			}
+
+			return Ok("Mentor added successfully");
+		}
 	}
 }
