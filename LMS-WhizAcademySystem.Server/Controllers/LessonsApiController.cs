7d2f0dceb553f9186502c9f42899d3e95383183c
Iwan <iwaniwanow0345@gmail.com>
2024-07-23 10:11:15 +0300

    lessons service used in api controller

--- LMS-WhizAcademySystem.Server/Controllers/LessonsApiController.cs
+++ LMS-WhizAcademySystem.Server/Controllers/LessonsApiController.cs
@@ -2,6 +2,7 @@
 {
 	using AutoMapper;
 	using LMS_WhizAcademySystem.Core.DTOs;
+	using LMS_WhizAcademySystem.Core.Services.Interfaces;
 	using LMS_WhizAcademySystem.Infrastructure.Data;
 	using LMS_WhizAcademySystem.Infrastructure.Models;
 	using Microsoft.AspNetCore.Mvc;
@@ -10,19 +11,17 @@
 	[ApiController]
 	public class LessonsApiController : ControllerBase
 	{
-		private readonly ApplicationDbContext _data;
-		private readonly IMapper _mapper;
+		private ILessonService _lessonService;
 
-		public LessonsApiController(ApplicationDbContext data, IMapper mapper)
+		public LessonsApiController(ILessonService lessonService)
 		{
-			_data = data;
-			_mapper = mapper;
+			_lessonService = lessonService;
 		}
 
 		[HttpPost("add")] // api/event/add
-		public IActionResult Post([FromBody] LessonFormDTO eventForm)
+		public async Task<IActionResult> Post([FromBody] LessonFormDTO model)
 		{
-			if (eventForm == null)
+			if (model == null)
 			{
 				return BadRequest("Event form is null.");
 			}
@@ -34,20 +33,7 @@
 
 			try
 			{
-				var eventModel = _mapper.Map<Lesson>(eventForm);
-
-				var mentor = _data.Mentors.FirstOrDefault(x => x.Email == eventForm.MentorEmail);
-				var student = _data.Students.FirstOrDefault(x => x.Email == eventForm.StudentEmail);
-
-				if (mentor == null || student == null)
-				{
-					return BadRequest();
-				}
-
-				eventModel.StudentId = 1;
-				eventModel.MentorId = 1;
-				//_data.Events.Add(eventModel);
-				//_data.SaveChanges(); // Ensure changes are saved to the database
+				await _lessonService.Add(model);
 			}
 			catch (Exception)
 			{
