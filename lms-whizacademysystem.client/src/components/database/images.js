f832033bc75fcd07bd727ea8e0692475d4a1b524
Iwan <iwaniwanow0345@gmail.com>
2024-07-23 09:20:29 +0300

    image routing in homepage

--- lms-whizacademysystem.client/src/components/database/images.js
+++ lms-whizacademysystem.client/src/components/database/images.js
@@ -9,7 +9,7 @@ import progress from '../../assets/images/graffits/14.png'
 const images = [
     { id: 1, src: mentor, link: '/mentors', information: 'Ментори' },
     { id: 2, src: student, link: '/students', information: 'Обучаеми' },
-    { id: 3, src: lesson, link: '/page3', information: 'График' },
+    { id: 3, src: lesson, link: '/calendar', information: 'График' },
     { id: 4, src: relative, link: '/page4', information: 'Разплащатели' },
     { id: 5, src: payment, link: '/payment', information: 'Плащания' },
     { id: 6, src: progress, link: '/page6', information: 'Напредък' },
