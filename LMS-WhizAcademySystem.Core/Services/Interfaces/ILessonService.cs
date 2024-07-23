0ddc640de17607b60b51f5edbf82c220dbc3f21a
Iwan <iwaniwanow0345@gmail.com>
2024-07-23 10:10:44 +0300

    lessons service was created, also add method was implemented

---
+++ LMS-WhizAcademySystem.Core/Services/Interfaces/ILessonService.cs
@@ -0,0 +1,9 @@
+﻿namespace LMS_WhizAcademySystem.Core.Services.Interfaces
+{
+	using LMS_WhizAcademySystem.Core.DTOs;
+
+	public interface ILessonService
+	{
+		Task Add(LessonFormDTO model);
+	}
+}
---
+++ LMS-WhizAcademySystem.Core/Services/LessonService.cs
@@ -0,0 +1,45 @@
+﻿namespace LMS_WhizAcademySystem.Core.Services
+{
+	using global::AutoMapper;
+	using LMS_WhizAcademySystem.Core.DTOs;
+	using LMS_WhizAcademySystem.Core.Services.Interfaces;
+	using LMS_WhizAcademySystem.Infrastructure.Data;
+	using LMS_WhizAcademySystem.Infrastructure.Models;
+
+	public class LessonService : ILessonService
+	{
+		private readonly ApplicationDbContext _data;
+		private readonly IMapper _mapper;
+		private readonly IStudentService _studentService;
+		private readonly IMentorService _mentorService;
+		public LessonService(ApplicationDbContext data, IMapper mapper, IStudentService studentService, IMentorService mentorService)
+		{
+			_data = data;
+			_mapper = mapper;
+			_studentService = studentService;
+			_mentorService = mentorService;
+		}
+
+		public async Task Add(LessonFormDTO model)
+		{
+			var mentor = await _mentorService.GetByEmail(model.MentorEmail);
+			var student = await _studentService.GetStudentByEmail(model.StudentEmail);
+
+			if (mentor == null || student == null || string.IsNullOrWhiteSpace(model.Title))
+			{
+				throw new Exception();
+			}
+
+			var lessons = _mapper.Map<Lesson>(model);
+
+
+			lessons.StudentId = student.Id;
+			lessons.MentorId = mentor.Id;
+
+
+			await _data.Lessons.AddAsync(lessons);
+			await _data.SaveChangesAsync(); // Ensure changes are saved to the database
+		}
+	}
+}
+
