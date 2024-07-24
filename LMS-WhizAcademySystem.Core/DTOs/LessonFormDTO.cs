f1e7ac178245cc8f65131211e2c0542c91b66d56
Iwan <iwaniwanow0345@gmail.com>
2024-07-24 16:42:03 +0300

    LessonFormDTO changed

--- LMS-WhizAcademySystem.Core/DTOs/LessonFormDTO.cs
+++ LMS-WhizAcademySystem.Core/DTOs/LessonFormDTO.cs
@@ -1,12 +1,20 @@
-﻿namespace LMS_WhizAcademySystem.Core.DTOs
+﻿using LMS_WhizAcademySystem.Infrastructure.Models;
+
+namespace LMS_WhizAcademySystem.Core.DTOs
 {
-	public class LessonFormDTO
-	{
-		public string? Title { get; set; }
-		public string? StudentEmail { get; set; }
-		public string? MentorEmail { get; set; }
-		public string? Description { get; set; }
-		public DateTime StartingDate { get; set; }
-		public DateTime EndingDate { get; set; }
-	}
+    public class LessonFormDTO
+    {
+        public string? Title { get; set; }
+        public string? StudentEmail { get; set; }
+        public string? MentorEmail { get; set; }
+        public string? Description { get; set; }
+        public string? StartingDateString { get; set; }
+        public string? EndingDateString { get; set; }
+
+        public DateTime? EndingDate { get; set; }
+        public DateTime? StartingDate { get; set; }
+
+        public Student? Student { get; set; }
+        public Mentor? Mentor { get; set; }
+    }
 }
