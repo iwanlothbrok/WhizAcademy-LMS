8ccd7e37a70d141099069a744e35447579cd7723
Iwan <iwaniwanow0345@gmail.com>
2024-07-26 16:21:14 +0300

    relative component was created

---
+++ LMS-WhizAcademySystem.Server/Controllers/RelativeController.cs
@@ -0,0 +1,38 @@
+﻿namespace LMS_WhizAcademySystem.Server.Controllers
+{
+    using LMS_WhizAcademySystem.Core.DTOs;
+    using LMS_WhizAcademySystem.Core.Services.Interfaces;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/relatives")]
+    [ApiController]
+    public class RelativeController(IRelativeService relativeService) : ControllerBase
+    {
+        private readonly IRelativeService _relativeService = relativeService;
+
+        [HttpPost("add")] // api/mentors/add FROM QUERY
+        public IActionResult Post([FromBody] RelativeFormDto relative) //[
+        {
+            if (relative == null)
+            {
+                return BadRequest("Relative is null.");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("Invalid ModelState.");
+            }
+
+            try
+            {
+                _relativeService.Add(relative);
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+
+            return Ok("Mentor added successfully");
+        }
+    }
+}
