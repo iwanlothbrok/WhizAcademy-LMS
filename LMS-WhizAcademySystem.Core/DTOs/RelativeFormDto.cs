221398d75cd31347972da84cecc3b8893f1a219b
Iwan <iwaniwanow0345@gmail.com>
2024-07-26 16:20:22 +0300

    relative form dto

---
+++ LMS-WhizAcademySystem.Core/DTOs/RelativeFormDto.cs
@@ -0,0 +1,8 @@
+﻿namespace LMS_WhizAcademySystem.Core.DTOs
+{
+    public class RelativeFormDto
+    {
+        public string? Name { get; set; }
+        public string? StudentId { get; set; }
+    }
+}
