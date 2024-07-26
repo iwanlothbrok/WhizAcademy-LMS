3de2828fd4c2519b1479d6d536ecec9a461aee34
Iwan <iwaniwanow0345@gmail.com>
2024-07-26 16:21:41 +0300

    used relative service and relative component

--- LMS-WhizAcademySystem.Server/Extensions/ServiceCollectionExtensions.cs
+++ LMS-WhizAcademySystem.Server/Extensions/ServiceCollectionExtensions.cs
@@ -1,20 +1,22 @@
 ﻿namespace LMS_WhizAcademySystem.Server.Extensions
 {
-	using LMS_WhizAcademySystem.Core.Services;
-	using LMS_WhizAcademySystem.Core.Services.Interfaces;
+    using LMS_WhizAcademySystem.Core.Services;
+    using LMS_WhizAcademySystem.Core.Services.Interfaces;
 
-	public static class ServiceCollectionExtensions
-	{
-		public static IServiceCollection AddApplicationServices(this IServiceCollection services)
-		{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
 
-			services.AddScoped<IMentorService, MentorService>();
-			services.AddScoped<IStudentService, StudentService>();
-			services.AddScoped<IPaymentService, PaymentService>();
-			services.AddScoped<ILessonService, LessonService>();
-			services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+            services.AddScoped<IMentorService, MentorService>();
+            services.AddScoped<IStudentService, StudentService>();
+            services.AddScoped<IPaymentService, PaymentService>();
+            services.AddScoped<ILessonService, LessonService>();
+            services.AddScoped<IRelativeService, RelativeService>();
 
-			return services;
-		}
-	}
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+            return services;
+        }
+    }
 }
--- lms-whizacademysystem.client/src/components/database/images.js
+++ lms-whizacademysystem.client/src/components/database/images.js
@@ -1,18 +1,18 @@
-import { info } from 'autoprefixer'
 import mentor from '../../assets/images/graffits/9.png'
 import lessonsPage from '../../assets/images/lessons/9.png'
 
 import student from '../../assets/images/students/21.png'
 import lesson from '../../assets/images/graffits/11.png'
-import relative from '../../assets/images/graffits/12.png'
 import payment from '../../assets/images/payment/22.png'
 import progress from '../../assets/images/graffits/14.png'
+
+import relative from '../../assets/images/students/01.png'
 
 const images = [
     { id: 1, src: mentor, link: '/mentors', information: 'Ментори' },
     { id: 2, src: student, link: '/students', information: 'Обучаеми' },
     { id: 3, src: lessonsPage, link: '/lessons', information: 'График' },
-    { id: 4, src: relative, link: '/page4', information: 'Разплащатели' },
+    { id: 4, src: relative, link: '/relative/add', information: 'Разплащатели' },
     { id: 5, src: payment, link: '/payment', information: 'Плащания' },
     { id: 6, src: progress, link: '/page6', information: 'Напредък' },
 ];
