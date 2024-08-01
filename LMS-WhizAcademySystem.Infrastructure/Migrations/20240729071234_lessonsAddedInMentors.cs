9fddff45dddb7030ddd52a4947f236c1211622ef
Iwan <iwaniwanow0345@gmail.com>
2024-08-01 09:46:38 +0300

    fk added in mentors

---
+++ LMS-WhizAcademySystem.Infrastructure/Migrations/20240729071234_lessonsAddedInMentors.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LMS_WhizAcademySystem.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class lessonsAddedInMentors : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Students_Mentors_MentorId",
+                table: "Students");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Students_Mentors_MentorId",
+                table: "Students",
+                column: "MentorId",
+                principalTable: "Mentors",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Students_Mentors_MentorId",
+                table: "Students");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Students_Mentors_MentorId",
+                table: "Students",
+                column: "MentorId",
+                principalTable: "Mentors",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
